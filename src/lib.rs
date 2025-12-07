b5f8b9c1f44bd14bea8d9bc6d55a5b80ae1cafa5
sharmuz <muzher@gmail.com>
2025-12-07 09:54:31 +0000

    Test and implement adding Book to Library

--- src/lib.rs
+++ src/lib.rs
@@ -26,7 +26,8 @@ impl Library {
     }
 
     pub fn add(&mut self, title: &str, author: &str, isbn: Option<&str>) {
-        ()
+        let book = Book::new(title, author, isbn);
+        self.books.push(book);
     }
 
     pub fn show(&self) -> String {
@@ -41,5 +42,22 @@ mod tests {
     #[test]
     fn test_new_creates_new_book() {
         let _book = Book::new("paradise lost", "milton", Some("97812345"));
+    }
+
+    #[test]
+    fn test_add_adds_new_book_without_isbn() {
+        let mut my_lib = Library::new();
+        let my_book = Book {
+            title: "the tale of genji".to_owned(),
+            author: "murasaki shikibu".to_owned(),
+            isbn: None,
+        };
+        let expected = Library {
+            books: vec![my_book],
+        };
+
+        my_lib.add("the tale of genji", "murasaki shikibu", None);
+
+        assert_eq!(my_lib, expected);
     }
 }
