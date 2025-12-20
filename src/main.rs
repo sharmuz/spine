ff0ee741dd7394a313c53c905030ee961b2d7fd9
sharmuz <muzher@gmail.com>
2025-12-20 09:13:01 +0000

    Load existing library in main else save new

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
+use std::{error::Error, path::Path};
+
 use spine::Library;
 
-fn main() {
-    let mut my_lib = Library::new();
-    my_lib.add("hadji murat", "leo tolstoy", Some("9780123456789"), None);
-    my_lib.add(
-        "norwegian wood",
-        "haruki murakami",
-        None,
-        Some(spine::Status::Read),
-    );
+fn main() -> Result<(), Box<dyn Error>> {
+    let path = Path::new("spine.json");
+
+    let my_lib= if path.exists() {
+        Library::open(&path)?
+    } else {
+        let mut my_lib = Library::new();
+        my_lib.add("hadji murat", "leo tolstoy", Some("9780123456789"), None);
+        my_lib.add(
+            "norwegian wood",
+            "haruki murakami",
+            None,
+            Some(spine::Status::Read),
+        );
+        my_lib.save(&path)?;
+        my_lib
+    };
     println!("Books in your library:\n\n{}", my_lib.show());
+    Ok(())
 }
