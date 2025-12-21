1af925b75dd9d2eb5281095d19878b488cafba35
sharmuz <muzher@gmail.com>
2025-12-21 10:36:52 +0000

    Rewrite integration test using assert_cmd

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,18 +1,24 @@
-use std::{fs, process::Command};
+use std::fs;
+
+use assert_cmd::cargo::cargo_bin_cmd;
+use predicates::prelude::*;
 
 #[test]
 fn spine_add_adds_new_book_to_existing_library() {
     fs::copy("tests/data/single_book.json", "tests/data/spine.json").unwrap();
 
-    let cmd = Command::new("cargo")
-        .args(["run", "--"])
+    let mut cmd = cargo_bin_cmd!("spine");
+
+    let assert = cmd
         .args(["add", "--reading", "norwegian wood", "haruki murakami"])
         .current_dir("tests/data")
-        .output()
-        .expect("Failed to add book");
-
-    assert!(cmd.status.success());
-    assert_eq!(cmd.stdout, b"Book added!\n");
+        .assert();
+    
+    assert
+        .success()
+        .append_context("main", "failed to add book")
+        .stdout(predicate::str::contains("Book added!"))
+        .append_context("main", "wrong output");
 
     fs::remove_file("tests/data/spine.json").unwrap();
 }
