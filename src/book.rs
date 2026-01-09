3f1ea2d5f6f815da161ee866acc7569a028f5d8a
sharmuz <muzher@gmail.com>
2026-01-09 15:42:24 +0000

    Add enum ISBN

--- src/book.rs
+++ src/book.rs
@@ -12,7 +12,7 @@ pub struct Book {
     pub id: Uuid,
     pub title: String,
     pub author: String,
-    pub isbn: Option<String>,
+    pub isbn: Option<Isbn>,
     pub status: Status,
     pub tags: Vec<String>,
 }
@@ -20,6 +20,53 @@ pub struct Book {
 impl Display for Book {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{}, {}", self.title, self.author)
+    }
+}
+
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+pub enum Isbn {
+    Isbn10(String),
+    Isbn13(String),
+}
+
+impl Isbn {
+    pub fn as_str(&self) -> &str {
+        match self {
+            Isbn::Isbn10(s) => s,
+            Isbn::Isbn13(s) => s,
+        }
+    }
+}
+
+impl FromStr for Isbn {
+    type Err = io::Error;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let norm = s.replace(" ", "").replace("-", "");
+        let mut rev = norm.chars().rev();
+        let last = rev.next();
+        for c in rev {
+            if !c.is_digit(10) {
+                return Err(io::Error::new(
+                    io::ErrorKind::InvalidInput,
+                    "Invalid ISBN: must contain only digits separated by hyphens or spaces.",
+                ));
+            }
+        }
+        if norm.len() == 10
+            && (last.is_some_and(|c| c.is_digit(10) || c.eq_ignore_ascii_case(&'x')))
+        {
+            return Ok(Isbn::Isbn10(norm));
+        }
+        if norm.len() == 13
+            && (&norm[..3] == "978" || &norm[..3] == "979")
+            && last.is_some_and(|c| c.is_digit(10))
+        {
+            return Ok(Isbn::Isbn13(norm));
+        }
+        Err(io::Error::new(
+            io::ErrorKind::InvalidInput,
+            "Invalid ISBN: must be either 10 digits or 13 digits with a prefix of 978/979.",
+        ))
     }
 }
 
