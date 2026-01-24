b80a2d684efce468dc5bc8255650d21fcf45c7e1
sharmuz <muzher@gmail.com>
2026-01-24 08:45:36 +0000

    Add struct Author

--- src/book.rs
+++ src/book.rs
@@ -23,6 +23,40 @@ impl Display for Book {
     }
 }
 
+#[derive(Clone, Debug, Default, Deserialize, Eq, PartialEq, Serialize)]
+pub struct Author {
+    pub first_name: String,
+    pub surname: String,
+}
+
+impl Display for Author {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        if self.surname.is_empty() {
+            write!(f, "{}", self.first_name)
+        } else {
+            write!(f, "{} {}", self.first_name, self.surname)
+        }
+    }
+}
+
+impl FromStr for Author {
+    type Err = io::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut names = s.split_whitespace();
+        let first = names.next().ok_or(io::Error::new(
+            io::ErrorKind::InvalidInput,
+            "Invalid author name provided.",
+        ))?;
+        let surname = names.collect::<Vec<&str>>().join(" ");
+
+        Ok(Self {
+            first_name: first.to_owned(),
+            surname,
+        })
+    }
+}
+
 #[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
 pub enum Isbn {
     Isbn10(String),
@@ -40,6 +74,7 @@ impl Isbn {
 
 impl FromStr for Isbn {
     type Err = io::Error;
+
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let norm = s.replace(" ", "").replace("-", "");
         let mut rev = norm.chars().rev();
@@ -80,6 +115,7 @@ pub enum Status {
 
 impl FromStr for Status {
     type Err = io::Error;
+
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s.trim().to_lowercase().as_str() {
             "want" => Ok(Status::Want),
@@ -90,5 +126,35 @@ impl FromStr for Status {
                 "Invalid status: expected 'want', 'reading', or 'read'",
             )),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn author_creates_from_three_names() {
+        let author = Author::from_str("ursula le guin").unwrap();
+        
+        assert_eq!(author.first_name, "ursula");
+        assert_eq!(author.surname, "le guin");
+        assert_eq!(author.to_string(), "ursula le guin");
+    }
+
+    #[test]
+    fn author_creates_from_only_first_name() {
+        let author = Author::from_str("confucius").unwrap();
+
+        assert_eq!(author.first_name, "confucius");
+        assert_eq!(author.surname, "");
+        assert_eq!(author.to_string(), "confucius");
+    }
+
+    #[test]
+    fn author_throws_error_if_no_names() {
+        let err = Author::from_str("").unwrap_err();
+
+        assert!(err.to_string().contains("Invalid author name"));
     }
 }
