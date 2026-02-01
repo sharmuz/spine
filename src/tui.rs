0d74ab3c986f99e35fa8f9abbcd5dfcaea27c545
sharmuz <muzher@gmail.com>
2026-02-01 13:36:50 +0000

    Add TUI rendering and event-handling logic

--- src/tui.rs
+++ src/tui.rs
@@ -1,7 +1,7 @@
 use std::io;
 
 use ratatui::{
-    DefaultTerminal, Frame, Terminal,
+    DefaultTerminal, Frame,
     buffer::Buffer,
     crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind},
     layout::Rect,
@@ -24,6 +24,80 @@ impl Tui {
 
     pub fn run(mut self, mut terminal: DefaultTerminal) -> io::Result<()> {
         self.is_running = true;
-        todo!()
+        while self.is_running {
+            terminal.draw(|frame| self.draw(frame))?;
+            self.handle_events()?;
+        }
+        Ok(())
+    }
+
+    fn draw(&self, frame: &mut Frame) {
+        frame.render_widget(self, frame.area());
+    }
+
+    fn handle_events(&mut self) -> io::Result<()> {
+        match event::read()? {
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                self.handle_key_event(key_event)
+            }
+            _ => {}
+        };
+        Ok(())
+    }
+
+    fn handle_key_event(&mut self, key: KeyEvent) {
+        match (key.modifiers, key.code) {
+            (_, KeyCode::Esc) => self.quit(),
+            //
+            _ => {}
+        }
+    }
+
+    fn quit(&mut self) {
+        self.is_running = false;
+    }
+}
+
+impl Widget for &Tui {
+    fn render(self, area: Rect, buf: &mut Buffer)
+    where
+        Self: Sized,
+    {
+        let title = Line::from(" Spine - Your Books ".bold());
+        let instructions = Line::from(vec![
+            " Move up ".into(),
+            "<Up>".blue().bold(),
+            " Move down ".into(),
+            "<Right>".blue().bold(),
+            " Quit ".into(),
+            "<Esc> ".blue().bold(),
+        ]);
+        let block = Block::bordered()
+            .title(title.centered())
+            .title_bottom(instructions.centered())
+            .border_set(border::THICK);
+        let book_text = Text::from(vec![Line::from(vec![
+            "1984".into(),
+            " | ".into(),
+            "George Orwell".into(),
+        ])]);
+
+        Paragraph::new(book_text)
+            .centered()
+            .block(block)
+            .render(area, buf);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn handle_key_event_quits_on_esc() {
+        let mut tui = Tui::new();
+        tui.handle_key_event(KeyCode::Esc.into());
+
+        assert!(!tui.is_running);
     }
 }
