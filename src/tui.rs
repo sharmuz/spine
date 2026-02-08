6f06929e36386d7a2bc7620927f2c0596f0ed718
sharmuz <muzher@gmail.com>
2026-02-08 09:45:33 +0000

    Add cursor to allow book selection

--- src/tui.rs
+++ src/tui.rs
@@ -7,20 +7,24 @@ use ratatui::{
     layout::Rect,
     style::Stylize,
     symbols::border,
-    text::{Line, Text},
-    widgets::{Block, Paragraph, Widget},
+    text::Line,
+    widgets::{Block, List, ListItem, Widget},
 };
 
 use crate::Library;
 
 #[derive(Debug, Default)]
 pub struct Tui {
-    library: Library,
     is_running: bool,
+    library: Library,
+    cursor: usize,
+    scroll_offset: usize,
 }
 
 enum Message {
     Quit,
+    CursorUp,
+    CursorDown,
 }
 
 impl Tui {
@@ -35,7 +39,7 @@ impl Tui {
 
         Ok(Self {
             library: my_lib,
-            is_running: false,
+            ..Default::default()
         })
     }
 
@@ -66,6 +70,8 @@ impl Tui {
     fn handle_key_event(&mut self, key: KeyEvent) -> Option<Message> {
         match (key.modifiers, key.code) {
             (_, KeyCode::Esc) => Some(Message::Quit),
+            (_, KeyCode::Up) => Some(Message::CursorUp),
+            (_, KeyCode::Down) => Some(Message::CursorDown),
             _ => None,
         }
     }
@@ -73,6 +79,10 @@ impl Tui {
     fn update(&mut self, msg: Message) {
         match msg {
             Message::Quit => self.is_running = false,
+            Message::CursorUp => self.cursor = self.cursor.saturating_sub(1),
+            Message::CursorDown => {
+                self.cursor = (self.cursor + 1).min(self.library.all().len().saturating_sub(1))
+            }
         }
     }
 }
@@ -87,7 +97,7 @@ impl Widget for &Tui {
             " Move up ".into(),
             "<Up>".blue().bold(),
             " Move down ".into(),
-            "<Right>".blue().bold(),
+            "<Down>".blue().bold(),
             " Quit ".into(),
             "<Esc> ".blue().bold(),
         ]);
@@ -99,13 +109,14 @@ impl Widget for &Tui {
         let books = self
             .library
             .all()
-            .map(|b| Line::from(b.to_string()))
-            .collect::<Vec<Line>>();
+            .enumerate()
+            .skip(self.scroll_offset)
+            .take(usize::from(area.height))
+            .map(|(i, b)| (i, ListItem::from(b.to_string())))
+            .map(|(i, t)| if i == self.cursor { t.green() } else { t })
+            .collect::<List>();
 
-        Paragraph::new(Text::from(books))
-            .centered()
-            .block(block)
-            .render(area, buf);
+        books.block(block).render(area, buf);
     }
 }
 
