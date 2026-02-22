daf09ca9e19808e212b07361601cb8b4670780ef
sharmuz <muzher@gmail.com>
2026-02-22 15:23:16 +0000

    Filter Library on key press

--- src/tui.rs
+++ src/tui.rs
@@ -1,4 +1,4 @@
-use std::{io, path::Path};
+use std::{collections::HashSet, io, path::Path};
 
 use ratatui::{
     DefaultTerminal, Frame,
@@ -10,8 +10,9 @@ use ratatui::{
     text::Line,
     widgets::{Block, List, ListItem, Widget},
 };
+use uuid::Uuid;
 
-use crate::Library;
+use crate::{Library, LibrarySearch, Status};
 
 #[derive(Debug, Default)]
 pub struct Tui {
@@ -20,6 +21,7 @@ pub struct Tui {
     cursor: usize,
     scroll_offset: usize,
     num_visible: usize,
+    filtered: Vec<Uuid>,
 }
 
 enum Message {
@@ -29,6 +31,7 @@ enum Message {
     CursorDown,
     PageUp,
     PageDown,
+    ApplyFilter,
 }
 
 impl Tui {
@@ -40,10 +43,12 @@ impl Tui {
         } else {
             Library::new()
         };
+        let all_ids = my_lib.all().map(|b| b.id).collect();
 
         Ok(Self {
             library: my_lib,
             num_visible: term_size.height.saturating_sub(2).into(),
+            filtered: all_ids,
             ..Default::default()
         })
     }
@@ -80,6 +85,7 @@ impl Tui {
             (_, KeyCode::Down) => Some(Message::CursorDown),
             (_, KeyCode::PageUp) => Some(Message::PageUp),
             (_, KeyCode::PageDown) => Some(Message::PageDown),
+            (_, KeyCode::Char('w')) => Some(Message::ApplyFilter),
             _ => None,
         }
     }
@@ -92,6 +98,7 @@ impl Tui {
             Message::CursorDown => self.move_cursor_down(),
             Message::PageUp => self.move_page_up(),
             Message::PageDown => self.move_page_down(),
+            Message::ApplyFilter => self.apply_filter(),
         }
     }
 
@@ -107,11 +114,11 @@ impl Tui {
     fn move_cursor_down(&mut self) {
         let is_last_visible =
             self.cursor == (self.scroll_offset + self.num_visible).saturating_sub(1);
-        let is_last_overall = self.cursor == self.library.all().len().saturating_sub(1);
+        let is_last_overall = self.cursor == self.filtered.len().saturating_sub(1);
         if is_last_visible && !is_last_overall {
             self.scroll_offset += 1;
         }
-        self.cursor = (self.cursor + 1).min(self.library.all().len().saturating_sub(1));
+        self.cursor = (self.cursor + 1).min(self.filtered.len().saturating_sub(1));
     }
 
     fn move_page_up(&mut self) {
@@ -122,11 +129,21 @@ impl Tui {
 
     fn move_page_down(&mut self) {
         let top_next_page = self.scroll_offset + self.num_visible;
-        let top_last_full_page = self.library.all().len().saturating_sub(self.num_visible);
+        let top_last_full_page = self.filtered.len().saturating_sub(self.num_visible);
         self.scroll_offset = top_next_page.min(top_last_full_page);
 
         let next_page_cursor = self.cursor + self.num_visible;
-        self.cursor = next_page_cursor.min(self.library.all().len().saturating_sub(1));
+        self.cursor = next_page_cursor.min(self.filtered.len().saturating_sub(1));
+    }
+
+    fn apply_filter(&mut self) {
+        let filter = LibrarySearch {
+            status: Some(Status::Want),
+            ..Default::default()
+        };
+        self.filtered = self.library.search(&filter).map(|b| b.id).collect();
+        self.cursor = 0;
+        self.scroll_offset = 0;
     }
 }
 
@@ -149,9 +166,11 @@ impl Widget for &Tui {
             .title_bottom(instructions.centered())
             .border_set(border::THICK);
 
+        let filtered_set: HashSet<Uuid> = self.filtered.iter().copied().collect();
         let books = self
             .library
             .all()
+            .filter(|b| filtered_set.contains(&b.id))
             .enumerate()
             .skip(self.scroll_offset)
             .take(usize::from(area.height))
