7f42d8cdbf5fb2f54e24c972213ceb52bd40e451
sharmuz <muzher@gmail.com>
2026-03-30 09:52:00 +0100

    Run in CLI mode by default instead of TUI

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
 fn main() -> anyhow::Result<()> {
-    if std::env::args().any(|arg| arg == "--cli") {
-        let cli_args = std::env::args().filter(|arg| arg != "--cli");
-        spine::cli::main(cli_args)
-    } else {
+    if std::env::args().any(|arg| arg == "--tui") {
         let mut terminal = ratatui::init();
         let term_size = terminal.get_frame().area();
         let tui_result = spine::tui::Tui::new(term_size)
@@ -10,5 +7,8 @@ fn main() -> anyhow::Result<()> {
             .run(terminal);
         ratatui::restore();
         Ok(tui_result?)
+    } else {
+        let cli_args = std::env::args();
+        spine::cli::main(cli_args)
     }
 }
